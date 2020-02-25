b1469134be1cf175a42e27249f13c52f48bd305b
suhyunsim <cnc1106@naver.com>
2020-02-25 13:10:37 +0900

    POST 방식으로 회원가입 폼 데이터 전달
    회원가입 폼에서 데이터를 입력하고 submit 버튼을 클릭했을 때 서버에
    POST방식으로 데이터가 전송 되었는지 확인함

--- src/main/java/net/slipp/myslipp/user/UserController.java
+++ src/main/java/net/slipp/myslipp/user/UserController.java
@@ -2,15 +2,17 @@ package net.slipp.myslipp.user;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UserController {
 
-    @RequestMapping("/hell")
-    public String user(String name, Model model) {
-        System.out.println("name: " + name);
-        model.addAttribute("name", name);
+    @PostMapping("/users")
+    public String create(String userId, String password, String name, String email) {
+        System.out.println("userId: "+ userId + " password: " + password + " name: " + name + " email: " + email);
         return "welcome";
     }
 }
