52829372f6f51097d8c5339d7e3f723464c96ec5
suhyunsim <cnc1106@naver.com>
2020-02-26 01:33:58 +0900

    회원가입 구현
    회원가입 기능을 구현하기 위해 html 파일들을 동적으로 관리한다.
    가입한 회원 목록을 GET mapping으로 관리한다.

--- src/main/java/net/slipp/myslipp/user/UserController.java
+++ src/main/java/net/slipp/myslipp/user/UserController.java
@@ -7,12 +7,22 @@ import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class UserController {
+    private List<User> users = new ArrayList<User>();
 
     @PostMapping("/users")
     public String create(User user) {
         System.out.println("user: " + user);
-        return "welcome";
+        users.add(user);
+        return "redirect:/users/list";
+    }
+    @GetMapping("/users/list")
+    public String list(Model model) {
+        model.addAttribute("users", users);
+        return "list";
     }
 }
